3f96698d44f65c475c48459d8a07e60a5fdb7bca
Luis <2123100609@soy.utj.edu.mx>
2025-02-17 07:40:09 -0600

    first commit

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -25,7 +25,7 @@ class CategoryController extends Controller
         $request->validate([
             'name' => 'required|string|max:50',
             'tags' => 'required|array',
-            'description' => 'required|string|max:50'
+            'description' => 'required|string|max:200'
         ]);
 
         $categories = new Category();
